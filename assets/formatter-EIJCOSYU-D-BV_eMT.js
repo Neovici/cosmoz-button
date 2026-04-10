541eab4ed8eb6149b539962c3a1fa0bff9b57b7f
cristinecula <VirusZ@gmail.com>
2026-04-10 11:10:57 +0000

    Deploying to gh-pages from @ Neovici/cosmoz-button@9d8a4017874295c47172f09dd8f4b8967ab5c8e5 🚀

---
+++ assets/formatter-EIJCOSYU-D-BV_eMT.js
@@ -0,0 +1,1 @@
+import{d as i,b as m}from"./DocsRenderer-LL677BLK-Dw67QJgT.js";import{J as o}from"./iframe-BVQ3DQO0.js";import"./preload-helper-PPVm8Dsz.js";var t=o(m(),1),d=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{d as formatter};
